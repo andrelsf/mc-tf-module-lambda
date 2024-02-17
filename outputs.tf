3b772ed45c62d592fdcbd32ae06549083657ae8d
Andre Ferreira <andre.ferreira@multicode.dev.br>
2024-02-17 15:58:56 -0300

    Updated version providers for support python3.12

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_function_arn" {
   description = "(Output) Amazon Resource Name (ARN) of the function's"
-  value = aws_lambda_function.this.arn
+  value       = aws_lambda_function.this.arn
 }
 
 output "lambda_function_name" {
   description = "(Output) Unique name for your Lambda Function"
-  value = aws_lambda_function.this.function_name
+  value       = aws_lambda_function.this.function_name
 }
